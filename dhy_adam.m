8e0b44d7555121a70aa596e96c20feb5be4c8f6c
Hongyuan Du <du-hongyuan@outlook.com>
2018-03-11 19:25:10 +0800

    MDS initialization added.

--- dhy_adam.m
+++ dhy_adam.m
@@ -29,10 +29,24 @@ m = zeros(n, 2);
 v = zeros(n, 2);
 
 % initial
-if ini == 'rQuads'
+if strcmp(ini, 'rQuads')
 
     rQuads = find_quads(M);
-    [Cor,~] = Localization(rQuads,M);
+    [Cor,~] = Localization(rQuads, M);
+    
+elseif strcmp(ini, 'MDS')
+    
+    Cor = MSD(M, '2D');
+    Cor = Cor - Cor(1, :);
+    mo = sqrt(sum(Cor(2, :) .^ 2));
+    c = Cor(2, 1) / mo;
+    s = Cor(2, 2) / mo;
+    Cor(:, 1) = Cor(:, 1) * c + Cor(:, 2) * s;
+    Cor(:, 2) = Cor(:, 2) * c - Cor(:, 1) * s;
+    Cor(2, :) = [mo, 0];
+    if Cor(3, 2) < 0
+        Cor(:, 2) = -Cor(:, 2);
+    end
     
 end
 
@@ -47,10 +61,10 @@ while 1
     M_t = sqrt((Cor_square - 2 * (Cor * Cor.')) + Cor_square.');
     
     % Compute loss
-    L = 0.25 * sum(sum((M_t - M).^2));
+    L = 0.25 * sum(sum((M_t - M) .^ 2));
     
     % Compute gradients
-    M_t = M_t + eye(n);
+    M_t = M_t + eye(n); % Avoid divided by zero.
     
     dM = M_t - M;
     dx = Cor(:, 1) - Cor(:, 1).';
