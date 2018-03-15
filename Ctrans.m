9dddbe2ac5d7881b9d8693d02a491028316323ad
Hongyuan Du <du-hongyuan@outlook.com>
2018-03-15 17:15:59 +0800

    Ctrans.m added.

---
+++ Ctrans.m
@@ -0,0 +1,20 @@
+function Cor_a = Ctrans(Cor_r, Cor_ap)
+%{
+    Transfor relative coordinates to absolute coordinates.
+ 
+    Inputs:
+
+    - Cor_r: Relative coordinates, of shape (N, 2).
+
+    - Cor_ap: Absolute coordinates of anchor nodes, of shape (C, 3), the
+              3rd column consists of indexes of the anchor nodes.
+
+    Outputs:
+
+    - Cor_a: The absolute coordinates, of shape (N, 2).
+
+%}
+
+
+end
+
