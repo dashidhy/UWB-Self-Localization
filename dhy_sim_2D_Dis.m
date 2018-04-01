7dea9a834bf14b7497ca3c3c904a158e16f7114c
Hongyuan Du <du-hongyuan@outlook.com>
2018-04-01 22:16:54 +0800

    Add time record and a figure of distributed alg.

--- dhy_sim_2D_Dis.m
+++ dhy_sim_2D_Dis.m
@@ -2,7 +2,7 @@ clear all; close all; clc;
 
 % Simulation parameters
 num_nodes = 1000;
-n_std = 0.0;
+n_std = 0.1;
 
 % Generate ground truth (G.T.)
 Cor_gt = 100 * rand(num_nodes, 2);
@@ -18,9 +18,11 @@ noise = ((noise + noise.') / 2) .* (1- eye(num_nodes));
 M_sim = M_gt + noise;
 
 % Comupte coordinates
+t = tic;
 Cor_sim_r = dhy_MDS_Adam_2D_Dis(M_sim, 32, 5, 1e-5);
 Ind = [1, 2, 3, 4];
 [Cor_sim_a, count_c, ~] = dhy_Ctrans_2D(Cor_sim_r, Cor_gt(Ind, :), Ind, 1e-5);
+t_e = toc(t);
 
 % Some statistics
 bias = sum(sqrt(sum((Cor_sim_a - Cor_gt) .^ 2, 2))) / num_nodes;
@@ -32,5 +34,5 @@ plot(Cor_gt(Ind, 1), Cor_gt(Ind, 2), 'go', 'Marker', 'square', 'markersize', 12,
 plot(Cor_sim_a(:, 1), Cor_sim_a(:, 2), 'bx', 'markersize', 10); hold on;
 axis equal;
 axis([-0.5, 100.5, -0.5, 100.5]);
-legend('Ground Truth', 'Anchor nodes', 'Sim');
+legend('Ground Truth', 'Anchor nodes', ['Sim, ', num2str(t_e), ' s']);
 title([num2str(num_nodes), ' nodes; Noise std: ', num2str(n_std * 100), ' cm; Avg bias: ', num2str(bias * 100), ' cm']);
