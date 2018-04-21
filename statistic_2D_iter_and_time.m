54971a52730fe93b1a3875818ff10f93e3660e9c
Hongyuan Du <du-hongyuan@outlook.com>
2018-04-21 10:55:21 +0800

    Add 2D statistics.

--- statistic_2D_iter_and_time.m
+++ statistic_2D_iter_and_time.m
@@ -7,7 +7,7 @@ range = 50;
 num_nodes = 10:5:100;
 noise_std = 0.1;
 
-num_rep = 500;
+num_rep = 5000;
 iter_array = zeros(1, length(num_nodes));
 time_array = zeros(1, length(num_nodes));
 
@@ -16,7 +16,8 @@ for i = 1:length(num_nodes)
     iter_t = 0;
     time_t = 0;
     
-    for j = 1:num_rep
+    j = 0;
+    while j < num_rep
         
         try
             
@@ -32,6 +33,8 @@ for i = 1:length(num_nodes)
             
         end
         
+        j = j + 1;
+        
     end
     
     iter_array(i) = iter_t ./ num_rep;
@@ -46,8 +49,10 @@ figure;
 plot(num_nodes, iter_array);
 xlabel('Number of nodes')
 ylabel(['Number of iterrations (', num2str(num_rep), ' times average)'])
+title('Iteration efficiency')
 
 figure;
 plot(num_nodes, time_array);
 xlabel('Number of nodes')
 ylabel(['Time consumed (', num2str(num_rep), ' times average)'])
+title('Time efficiency')
