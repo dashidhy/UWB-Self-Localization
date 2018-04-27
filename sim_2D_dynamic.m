62003069b1390498abe6fdc80593e6595b06def2
Hongyuan Du <du-hongyuan@outlook.com>
2018-04-27 12:57:07 +0800

    Add vanilla Kalman filter.

--- sim_2D_dynamic.m
+++ sim_2D_dynamic.m
@@ -22,9 +22,16 @@ R = [cos(vel_r ./ frame), -sin(vel_r ./ frame); sin(vel_r ./ frame), cos(vel_r .
 R2 = [cos(vel_r2 ./ frame), -sin(vel_r2 ./ frame); sin(vel_r2 ./ frame), cos(vel_r2 ./ frame)];
 
 G = CNet(Num_nodes, Dim);
-h_fig = figure('Visible', 'off');
+h_fig = figure('Visible', 'on');
+
+r = Noise_std .^ 2;
+p = 0.05;
+x_k_1 = zeros(Num_nodes, Dim);
+x_k_2 = zeros(Num_nodes, Dim);
 
 for i = 1:(frame * sim_time)
+    
+    e = 0;
 
     Loc_gt = [Loc_anchor; Loc_tag; Loc_tag2];
     Cor_square = sum(Loc_gt .^ 2, 2);
@@ -36,7 +43,39 @@ for i = 1:(frame * sim_time)
     G.M = M_gt + noise;
     G.Loc_anchor = Loc_anchor;
     G.Index_anchor = Index_anchor;
-    G.Localize();
+    
+    try
+        
+        G.Localize();
+        
+    catch
+        
+        e = 1;
+        
+    end
+    
+    if i > 2
+        
+        x_k = 2 .* x_k_1 - x_k_2;
+        p = 4 * p;
+        
+        g = p / (p + r);
+        
+        if e == 1
+            
+            G.Loc_cp = x_k;
+            
+        else
+            
+            G.Loc_cp = g .* x_k + (1 - g) .* G.Loc_cp;
+            
+        end
+        p = (1 - g) * p;
+        
+    end
+    
+    x_k_2 = x_k_1;
+    x_k_1 = G.Loc_cp;
     
     box on;
     plot(Loc_anchor(:, 1), Loc_anchor(:, 2), 'go', 'Marker', 'square', 'markersize', 15, 'linewidth', 3.0); hold on;
@@ -45,7 +84,7 @@ for i = 1:(frame * sim_time)
     axis equal;
     axis([center(1) - 5.5, center(1) + 5.5, center(2) - 5.5, center(2) + 5.5]);
     set(legend('Anchor', 'GroundTruth', 'Sim'), 'position', [0.4666071458622102, 0.7867292461877003, 0.10666666475435094, 0.12019230446849893]);
-    title('Dynamic Network Demo');
+    title('Dynamic Network Demo (with vanilla Kalman Filter)');
     M(i) = getframe(h_fig);
     
     Loc_tag2 = (Loc_tag2 - Loc_tag) * R2;
@@ -58,3 +97,13 @@ for i = 1:(frame * sim_time)
 end
 
 box off;
+
+v = VideoWriter('Dynamic_Network_Simulation_Kalman2.avi');
+v.FrameRate = frame;
+v.open();
+for i = 1:length(M)
+    
+    v.writeVideo(M(i));
+    
+end
+v.close();
